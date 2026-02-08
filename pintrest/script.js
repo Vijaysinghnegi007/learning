56ee5c9951a169844cfdff99b47c3ef744a707cb
Vijaysinghnegi007 <vijaysinghnegi672@gmail.com>
2026-02-08 21:39:45 +0530

    pintrest

---
+++ pintrest/script.js
@@ -0,0 +1,18 @@
+let search = document.querySelector(".search");
+let searchInput = document.querySelector(".searchInput");
+
+
+let images =[
+    "https://images.unsplash.com/photo-1668293750324-bd77c1f08ca9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW5pbWV8ZW58MHx8MHx8fDA%3D",
+    "https://in.pinterest.com/pin/200902833372207851/",
+    "https://in.pinterest.com/pin/87538786502557312/",
+    "https://in.pinterest.com/pin/31877109858018620/",
+    "https://in.pinterest.com/pin/72690981481006490/",
+    "https://in.pinterest.com/pin/435652963976359686/",
+    "https://in.pinterest.com/pin/274227064806969830/",
+    "https://in.pinterest.com/pin/14777505022709603/",
+]
+search.addEventListener("click", () => {
+  searchInput.focus()
+});
+
